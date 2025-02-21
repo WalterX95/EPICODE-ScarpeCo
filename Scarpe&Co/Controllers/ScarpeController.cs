caa6927539d5b34dba007c7243a0f50a80bb6bb7
WalterX95 <walter_1995@hotmail.it>
2025-02-21 12:03:49 +0100

    Aggiungere i file di progetto.

---
+++ Scarpe&Co/Controllers/ScarpeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScarpeCo.Models; // Importa il Model
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScarpeCo.Controllers
+{
+    public class ScarpeController : Controller
+    {
+        private static List<Scarpa> scarpe = new List<Scarpa>
+        {
+            new Scarpa { Id = 1, Nome = "Nike Air Zoom", Prezzo = 120.00m, Descrizione = "Scarpa da tennis leggera e resistente.", Copertina = "nike_air_zoom.jpg", Img1 = "nike_air_zoom_1.jpg", Img2 = "nike_air_zoom_2.jpg" },
+            new Scarpa { Id = 2, Nome = "Adidas CourtJam", Prezzo = 95.00m, Descrizione = "Scarpa ideale per superfici dure.", Copertina = "adidas_courtjam.jpg", Img1 = "adidas_courtjam_1.jpg", Img2 = "adidas_courtjam_2.jpg" }
+        };
+
+        public IActionResult Index()
+        {
+            return View(scarpe);
+        }
+
+        public IActionResult Dettaglio(int id)
+        {
+            var scarpa = scarpe.FirstOrDefault(s => s.Id == id);
+            if (scarpa == null)
+            {
+                return NotFound();
+            }
+            return View(scarpa);
+        }
+
+        public IActionResult Modifica(int id)
+        {
+            var scarpa = scarpe.FirstOrDefault(s => s.Id == id);
+            if (scarpa == null)
+            {
+                return NotFound();
+            }
+            return View(scarpa);
+        }
+
+        [HttpPost]
+        public IActionResult Modifica(Scarpa scarpaModificata)
+        {
+            var scarpa = scarpe.FirstOrDefault(s => s.Id == scarpaModificata.Id);
+            if (scarpa != null)
+            {
+                scarpa.Nome = scarpaModificata.Nome;
+                scarpa.Prezzo = scarpaModificata.Prezzo;
+                scarpa.Descrizione = scarpaModificata.Descrizione;
+                scarpa.Copertina = scarpaModificata.Copertina;
+                scarpa.Img1 = scarpaModificata.Img1;
+                scarpa.Img2 = scarpaModificata.Img2;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Scarpe&Co/Models/Scarpa.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScarpeCo.Models
+{
+    public class Scarpa
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal Prezzo { get; set; }
+        public string? Descrizione { get; set; }
+        public string? Copertina { get; set; }
+        public string? Img1 { get; set; }
+        public string? Img2 { get; set; }
+
+        // Metodo per formattare il prezzo
+        public string PrezzoFormattato()
+        {
+            return Prezzo.ToString("C");
+        }
+    }
+}
+
